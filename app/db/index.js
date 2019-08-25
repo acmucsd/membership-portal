069b180692bccfbd7a35c3ed3e2041218a15c50e
Sumeet Bansal <sumeetbansal@gmail.com>
2019-08-25 16:39:39 -0400

    HTTPError -> HttpError

--- app/db/index.js
+++ app/db/index.js
@@ -22,9 +22,9 @@ const errorHandler = (err, req, res, next) => {
   if (!err || !(err instanceof Sequelize.Error)) return next(err);
   if (err instanceof Sequelize.ValidationError) {
     const message = `Validation Error: ${err.errors.map((e) => e.message).join('; ')}`;
-    return next(new error.HTTPError(err.name, 422, message));
+    return next(new error.HttpError(err.name, 422, message));
   }
-  return next(new error.HTTPError(err.name, 500, err.message));
+  return next(new error.HttpError(err.name, 500, err.message));
 };
 
 module.exports = { db, errorHandler };
