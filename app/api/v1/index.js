568401d8f81b7ba693e054ff3da20df09cdaf87b
Sumeet Bansal <sumeetbansal@gmail.com>
2019-09-24 18:30:03 -0700

    implement basic auth

--- app/api/v1/index.js
+++ app/api/v1/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { authenticated } = require('./auth');
 
 const router = express.Router();
 
-router.use('/hello', require('./hello').router);
+router.use('/hello', authenticated, require('./hello').router);
+router.use('/auth', require('./auth').router);
 
 module.exports = { router };
