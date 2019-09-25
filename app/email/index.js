7eb9a084b68ca860adb816ece6a1bc57bfa175ce
Jaden Padua <paduajaden@gmail.com>
2019-09-25 00:46:42 -0700

    Update linter fix script and resolve styling issues

--- app/email/index.js
+++ app/email/index.js
@@ -2,6 +2,7 @@ const sgMail = require('@sendgrid/mail');
 const config = require('../config');
 
 sgMail.setApiKey(config.email.apiKey);
+
 const sendEmail = (to, subject, text, html) => sgMail.send({ to, from: config.email.from, subject, text, html });
 
 module.exports = sendEmail;
