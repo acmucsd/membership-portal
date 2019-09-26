5cc8e42aea7df217dec50614c0c592b9ce4bf96b
Sumeet Bansal <sumeetbansal@gmail.com>
2019-09-25 18:29:06 -0700

    added newline in config

--- app/config/index.js
+++ app/config/index.js
@@ -30,6 +30,7 @@ module.exports = {
     user: process.env.RDS_USER,
     password: process.env.RDS_PASSWORD,
   },
+
   email: {
     apiKey: process.env.SENDGRID_API_KEY,
     from: 'acmucsd.portal@gmail.com',
