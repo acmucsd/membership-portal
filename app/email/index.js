7cf31143f15555ec5cffbf70cd9100829d2cfaca
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-09-25 18:34:56 -0700

    created email module (#19)
    * Create sendEmail function and post route via sendgrid
    
    * Create basic email module/template for verification
    
    * Update send mail API key
    
    * Fix linter issues, Add linter fix script and Update README.md
    
    * Remove hardcode values and create config email object
    
    * Rename email/send-mail.js -> app/email/index.js
    
    * Update linter fix script and resolve styling issues
    
    * Change from email name to acmucsd.portal@gmail.com
    
    * Fix email export object and resolve styling issues
    
    * added newline in config

---
+++ app/email/index.js
@@ -0,0 +1,8 @@
+const sgMail = require('@sendgrid/mail');
+const config = require('../config');
+
+sgMail.setApiKey(config.email.apiKey);
+
+const sendEmail = (to, subject, text, html) => sgMail.send({ to, from: config.email.from, subject, text, html });
+
+module.exports = { sendEmail };
