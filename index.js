2149b3ecd9ce5fa8e2ba0eaea8272ee2e7aa39a1
Sumeet Bansal <sumeetbansal@gmail.com>
2020-04-26 17:27:09 -0700

    fixed broken import

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const server = require('./src');
 const { initializeAdmin } = require('./src/db');
+const config = require('./src/config');
 const log = require('./src/logger');
 
 // ensures an admin account is created
 initializeAdmin();
 
 // starts the server
-server.listen(server.config.port, () => {
-  log.info('Started server on port %d.', server.config.port);
+server.listen(config.port, () => {
+  log.info('Started server on port %d.', config.port);
 });
