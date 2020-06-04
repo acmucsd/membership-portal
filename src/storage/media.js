95d62b257c7b07ddd1e081954e4cc65885e3abc9
shravanhariharan2 <smhariha@ucsd.edu>
2020-06-04 15:23:38 -0700

    Add main banner option for file upload

--- src/storage/media.js
+++ src/storage/media.js
@@ -4,6 +4,7 @@ const error = require('../error');
 const mediaTypes = {
   EVENT_COVER: 1,
   PROFILE_PICTURE: 2,
+  MAIN_BANNER: 3,
 };
 
 const getMediaTypeConfig = (mediaType) => {
@@ -20,6 +21,12 @@ const getMediaTypeConfig = (mediaType) => {
         maxFileSize: config.file.MAX_PROFILE_PICTURE_FILE_SIZE,
         uploadPath: config.file.PROFILE_PICTURE_UPLOAD_PATH,
       };
+    case mediaTypes.BANNER_MAIN:
+      return {
+        type: mediaTypes.MAIN_BANNER,
+        maxFileSize: config.file.MAX_MAIN_BANNER_FILE_SIZE,
+        uploadPath: config.file.MAIN_BANNER_UPLOAD_PATH,
+      }
     default:
       throw new error.InternalServerError('Invalid media type for file');
   }
