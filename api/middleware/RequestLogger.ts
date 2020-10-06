bd92c8b7f37ab6f666f49a67f7f3481bbcb33313
Sumeet Bansal <sumeetbansal@gmail.com>
2020-10-05 22:32:32 -0700

    Update RequestLogger.ts

--- api/middleware/RequestLogger.ts
+++ api/middleware/RequestLogger.ts
@@ -1,13 +1,13 @@
 import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
+import * as crypto from 'crypto';
 import * as express from 'express';
 import * as moment from 'moment';
 import * as morgan from 'morgan';
-import * as uuid from 'uuid';
 
 @Middleware({ type: 'before' })
 export class RequestLogger implements ExpressMiddlewareInterface {
   async use(request: express.Request, response: express.Response, next?: express.NextFunction) {
-    request.trace = uuid.v4().split('-').pop();
+    request.trace = crypto.randomBytes(16).toString('hex');
     morgan.token('date', () => `${moment().format('llll')} PT`);
     morgan.token('ip', () => `[IP ${request.headers['x-forwarded-for'] || '-'}]`);
     morgan.token('trace', () => `[Trace ${request.trace}]`);
