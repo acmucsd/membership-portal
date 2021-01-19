b1a2c7b99b0bfa95f174165f86c4fb73eb6ea729
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-01-18 18:28:21 -0800

    Merge master changes

---
+++ migrations/0025-add-public-activity-scope-index.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner, TableIndex } from 'typeorm';
+
+export class AddActivityScopeIndex1608574428247 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createIndex('Activities',
+      new TableIndex({
+        name: 'visible_activities_by_user_index',
+        columnNames: ['user', 'scope'],
+        where: 'scope = \'PUBLIC\' OR scope = \'PRIVATE\'',
+      }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropIndex('Activities', 'visible_activities_by_user_index');
+  }
+}
