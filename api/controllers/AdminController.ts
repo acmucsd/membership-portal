5d7554dee237c03b81e52da9e8bdb8a604cb64c5
Shravan Hariharan <33337209+shravanhariharan2@users.noreply.github.com>
2021-01-22 11:46:10 -0800

    Add retroactive attendance admin route (#154)
    * Add retroactive attendance route
    
    * 2.8.0
    
    * Remove service constraints
    
    * Update services/AttendanceService.ts
    
    Co-authored-by: Sumeet Bansal <sumeetbansal@gmail.com>
    
    * Add activity description for retroactive attendance
    
    Co-authored-by: Sumeet Bansal <sumeetbansal@gmail.com>

--- api/controllers/AdminController.ts
+++ api/controllers/AdminController.ts
@@ -1,7 +1,11 @@
 import { JsonController, Post, UploadedFile, UseBefore, ForbiddenError, Body, Get } from 'routing-controllers';
 import { Inject } from 'typedi';
 import { UserAuthentication } from '../middleware/UserAuthentication';
-import { CreateBonusRequest, CreateMilestoneRequest } from '../validators/AdminControllerRequests';
+import {
+  CreateBonusRequest,
+  CreateMilestoneRequest,
+  SubmitAttendanceForUserRequest,
+} from '../validators/AdminControllerRequests';
 import {
   File,
   MediaType,
@@ -9,12 +13,14 @@ import {
   CreateBonusResponse,
   UploadBannerResponse,
   GetAllEmailsResponse,
+  AttendEventResponse,
 } from '../../types';
 import { AuthenticatedUser } from '../decorators/AuthenticatedUser';
 import UserAccountService from '../../services/UserAccountService';
 import StorageService from '../../services/StorageService';
 import PermissionsService from '../../services/PermissionsService';
 import { UserModel } from '../../models/UserModel';
+import AttendanceService from '../../services/AttendanceService';
 
 @UseBefore(UserAuthentication)
 @JsonController('/admin')
@@ -24,6 +30,9 @@ export class AdminController {
 
   @Inject()
   private userAccountService: UserAccountService;
+
+  @Inject()
+  private attendanceService: AttendanceService;
 
   @Get('/email')
   async getAllEmails(@AuthenticatedUser() user: UserModel): Promise<GetAllEmailsResponse> {
@@ -56,5 +65,14 @@ export class AdminController {
   ) file: File): Promise<UploadBannerResponse> {
     const banner = await this.storageService.upload(file, MediaType.BANNER, 'banner');
     return { error: null, banner };
+  }
+
+  @Post('/attendance')
+  async submitAttendanceForUser(@Body() submitAttendanceForUserRequest: SubmitAttendanceForUserRequest,
+    @AuthenticatedUser() currentUser: UserModel): Promise<AttendEventResponse> {
+    if (!PermissionsService.canSubmitAttendanceForUsers(currentUser)) throw new ForbiddenError();
+    const { user, event: eventToAttend, asStaff } = submitAttendanceForUserRequest;
+    const { event } = await this.attendanceService.submitAttendanceForUser(user, eventToAttend, asStaff, currentUser);
+    return { error: null, event };
   }
 }
