2231dc9739f089bf80eaef979caf746e3ac2a537
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-02-16 18:42:50 -0800

    Remove test container config

--- tests/index.ts
+++
@@ -1,7 +0,0 @@
-import 'reflect-metadata';
-import { useContainer as routingUseContainer } from 'routing-controllers';
-import { useContainer as ormUseContainer } from 'typeorm';
-import Container from 'typedi';
-
-routingUseContainer(Container);
-ormUseContainer(Container);
