bc43ff93fdb34d511f713cf39c3a5249201a1f9d
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-04-18 11:54:00 -0700

    Add rest of CRUD operation tests

--- tests/event.test.ts
+++ tests/event.test.ts
@@ -1,4 +1,7 @@
-import { DatabaseConnection } from "./data";
+import * as moment from 'moment';
+import { DatabaseConnection, EventFactory, PortalState, UserFactory } from './data';
+import { ControllerFactory } from './controllers';
+import { UserAccessType } from '../types';
 
 beforeAll(async () => {
   await DatabaseConnection.connect();
@@ -14,16 +17,88 @@ afterAll(async () => {
 });
 
 describe('event CRUD operations', () => {
-  test('events from the past, present, future, and all time can be pulled', async () => {
+  test('events from the past, future, and all time can be pulled', async () => {
+    const conn = await DatabaseConnection.get();
+    const [pastEvent] = EventFactory.with({
+      start: moment().subtract(1, 'day').toDate(),
+      end: moment().subtract(1, 'day').add(60, 'minutes').toDate(),
+    });
+    const [ongoingEvent] = EventFactory.with({
+      start: moment().subtract(30, 'minutes').toDate(),
+      end: moment().add(30, 'minutes').toDate(),
+    });
+    const [futureEvent] = EventFactory.with({
+      start: moment().add(1, 'day').toDate(),
+      end: moment().add(1, 'day').add(60, 'minutes').toDate(),
+    });
+    const [user] = UserFactory.create(1);
 
+    await new PortalState()
+      .createEvents([pastEvent, ongoingEvent, futureEvent])
+      .createUsers([user])
+      .write();
+
+    const eventController = ControllerFactory.event(conn);
+
+    const pastEventsResponse = await eventController.getPastEvents({}, user);
+    const futureEventsResponse = await eventController.getFutureEvents({}, user);
+    const allEventsResponse = await eventController.getAllEvents({}, user);
+
+    expect(pastEventsResponse.events).toEqual(
+      expect.arrayContaining([pastEvent.getPublicEvent()]),
+    );
+    expect(futureEventsResponse.events).toEqual(
+      expect.arrayContaining([
+        ongoingEvent.getPublicEvent(),
+        futureEvent.getPublicEvent(),
+      ]),
+    );
+    expect(allEventsResponse.events).toEqual(
+      expect.arrayContaining([
+        pastEvent.getPublicEvent(),
+        ongoingEvent.getPublicEvent(),
+        futureEvent.getPublicEvent(),
+      ]),
+    );
   });
 
   test('events can be created with unused attendance codes', async () => {
+    const conn = await DatabaseConnection.get();
+    const [event1] = EventFactory.with({ attendanceCode: 'code' });
+    const [event2] = EventFactory.with({ attendanceCode: 'different-code' });
+    const [admin] = UserFactory.with({ accessType: UserAccessType.ADMIN });
 
+    await new PortalState()
+      .createEvents([event1])
+      .createUsers([admin])
+      .write();
+
+    const createEventResponse = await ControllerFactory.event(conn).createEvent({ event: event2 }, admin);
+
+    expect(createEventResponse.error).toBeNull();
   });
 
   test('events cannot be created with duplicate attendance codes', async () => {
+    const conn = await DatabaseConnection.get();
+    const [event1] = EventFactory.with({ attendanceCode: 'code' });
+    const [event2] = EventFactory.with({ attendanceCode: 'code' });
+    const [admin] = UserFactory.with({ accessType: UserAccessType.ADMIN });
 
+    await new PortalState()
+      .createEvents([event1])
+      .createUsers([admin])
+      .write();
+
+    const eventController = ControllerFactory.event(conn);
+    const errorMessage = 'Attendance code has already been used';
+
+    await expect(
+      eventController.createEvent({ event: event2 }, admin),
+    ).rejects.toThrow(errorMessage);
+
+    const eventsResponse = await eventController.getAllEvents({}, admin);
+
+    expect(eventsResponse.events).toHaveLength(1);
   });
 });
 
@@ -37,7 +112,7 @@ describe('event feedback', () => {
   test('can be persisted and rewarded points when submitted for an event already attended', async () => {
 
   });
-  
+
   test('is rejected on submission to an event not attended', async () => {
 
   });
@@ -49,4 +124,4 @@ describe('event feedback', () => {
   test('is rejected if sent after 2 days of event completion', async () => {
 
   });
-})
+});
