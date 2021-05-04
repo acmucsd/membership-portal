37fcd221c871eac02d1907269ec99a8784dd1d80
Michael Shao <56284221+michl1001@users.noreply.github.com>
2021-05-04 00:49:21 -0400

    Added inline emailParam objects

--- tests/admin.test.ts
+++ tests/admin.test.ts
@@ -36,10 +36,7 @@ describe('retroactive attendance submission', () => {
 
     for (let u = 0; u < users.length; u += 1) {
       const user = users[u];
-      const userUuid = {
-        uuid: user.uuid,
-      };
-      const userResponse = await userController.getUser(userUuid, proxyUser);
+      const userResponse = await userController.getUser({ uuid: user.uuid }, proxyUser);
 
       expect(userResponse.user.points).toEqual(user.points + event.pointValue);
 
@@ -47,7 +44,7 @@ describe('retroactive attendance submission', () => {
       expect(attendanceResponse.attendances).toHaveLength(1);
       expect(attendanceResponse.attendances[0].event).toStrictEqual(event.getPublicEvent());
 
-      const activityResponse = await userController.getUserActivityStream(userUuid, proxyUser);
+      const activityResponse = await userController.getUserActivityStream({ uuid: user.uuid }, proxyUser);
 
       expect(activityResponse.activity).toHaveLength(2);
       expect(activityResponse.activity[1].pointsEarned).toEqual(event.pointValue);
@@ -77,11 +74,7 @@ describe('retroactive attendance submission', () => {
       proxyUser,
     );
 
-    const userUuid = {
-      uuid: user.uuid,
-    };
-
-    const userResponse = await userController.getUser(userUuid, proxyUser);
+    const userResponse = await userController.getUser({ uuid: user.uuid }, proxyUser);
     const attendanceResponse = await attendanceController.getAttendancesForCurrentUser(user);
     const activityResponse = await userController.getCurrentUserActivityStream(user);
 
@@ -113,15 +106,8 @@ describe('retroactive attendance submission', () => {
 
     await adminController.submitAttendanceForUsers(request, proxyUser);
 
-    const userUuid = {
-      uuid: user.uuid,
-    };
-    const staffUuid = {
-      uuid: staffUser.uuid,
-    };
-
-    const userResponse = await userController.getUser(userUuid, proxyUser);
-    const staffUserResponse = await userController.getUser(staffUuid, proxyUser);
+    const userResponse = await userController.getUser({ uuid: user.uuid }, proxyUser);
+    const staffUserResponse = await userController.getUser({ uuid: staffUser.uuid }, proxyUser);
     const activityResponse = await userController.getCurrentUserActivityStream(user);
     const staffActivityResponse = await userController.getCurrentUserActivityStream(staffUser);
 
