4affd4d774c89f05f19acc941d04431c4bdffb3e
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-05-24 16:02:58 -0700

    Improve error messages

--- error/messages.ts
+++ error/messages.ts
@@ -1,12 +1,12 @@
 export enum UserErrors {
   MULTIPLE_MERCH_OPTION_TYPES = 'Item cannot have multiple option types',
-  NO_ITEM_VARIANTS_ADD_OPTION = 'Item does not allow for multiple options',
+  NO_ITEM_VARIANTS_ADD_OPTION = 'Cannot add option to item with no variants',
   NO_ITEM_VARIANTS_BUT_MULTIPLE_OPTIONS = 'Item with variants disabled cannot have multiple options',
-  MERCH_COLLECTION_ORDERED_FROM = 'This collection has been ordered from',
-  MERCH_ITEM_ORDERED_FROM = 'This item has been ordered from',
-  MERCH_ITEM_OPTION_ORDERED_FROM = 'This item option has been ordered from',
+  MERCH_COLLECTION_ORDERED_FROM = 'This collection has been ordered from, and thus cannot be deleted',
+  MERCH_ITEM_ORDERED_FROM = 'This item has been ordered from, and thus cannot be deleted',
+  MERCH_ITEM_OPTION_ORDERED_FROM = 'This item option has been ordered from, and thus cannot be deleted',
   MERCH_ORDER_DOUBLE_FULFILLMENT = 'At least one order item marked to be fulfilled has already been fulfilled',
-  NOT_ENOUGH_CREDITS = 'You don\'t have enough credits',
+  NOT_ENOUGH_CREDITS = 'You don\'t have enough credits for this order',
 }
 
 export enum NotFoundErrors {
