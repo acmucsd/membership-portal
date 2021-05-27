539d35e9d5483b3e614f4330d28602ef0a29939a
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-05-27 14:33:16 -0700

    Remove redundant type

--- services/StorageService.ts
+++ services/StorageService.ts
@@ -4,9 +4,8 @@ import * as path from 'path';
 import * as multer from 'multer';
 import { InternalServerError } from 'routing-controllers';
 import { Config } from '../config';
-import { MediaType } from '../types';
+import { MediaType, File } from '../types';
 
-type File = Express.Multer.File;
 type FileOptions = multer.Options;
 
 interface MediaTypeConfig {
