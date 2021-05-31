c837c9adf8c35da3ebcf08a9ab2ebfcb36073591
Shravan Hariharan <33337209+shravanhariharan2@users.noreply.github.com>
2021-05-31 12:00:47 -0700

    Update error messages
    Co-authored-by: Sumeet Bansal <sumeetbansal@gmail.com>

--- error/messages.ts
+++ error/messages.ts
@@ -1,10 +1,10 @@
 export enum UserErrors {
   MULTIPLE_MERCH_OPTION_TYPES = 'Item cannot have multiple option types',
-  NO_ITEM_VARIANTS_ADD_OPTION = 'Cannot add option to item with no variants',
-  NO_ITEM_VARIANTS_BUT_MULTIPLE_OPTIONS = 'Item with variants disabled cannot have multiple options',
-  MERCH_COLLECTION_ORDERED_FROM = 'This collection has been ordered from, and thus cannot be deleted',
-  MERCH_ITEM_ORDERED_FROM = 'This item has been ordered from, and thus cannot be deleted',
-  MERCH_ITEM_OPTION_ORDERED_FROM = 'This item option has been ordered from, and thus cannot be deleted',
+  VARIANTS_DISABLED_ADD_OPTION = 'Cannot add option to items with variants disabled',
+  VARIANTS_DISABLED_MULTIPLE_OPTIONS = 'Items with variants disabled cannot have multiple options',
+  MERCH_COLLECTION_ORDERED_FROM = 'This collection has been ordered from and cannot be deleted',
+  MERCH_ITEM_ORDERED = 'This item has been ordered and cannot be deleted',
+  MERCH_ITEM_OPTION_ORDERED = 'This item option has been ordered and cannot be deleted',
   MERCH_ORDER_DOUBLE_FULFILLMENT = 'At least one order item marked to be fulfilled has already been fulfilled',
   NOT_ENOUGH_CREDITS = 'You don\'t have enough credits for this order',
 }
