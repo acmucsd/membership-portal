c8c5df60ca12c2fd4e73ccaf5c3a59c92eaf4908
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-06-19 21:35:11 -0700

    forgot to save lmao

--- error/index.ts
+++ error/index.ts
@@ -1,2 +1,26 @@
-export * from './handlers';
-export * from './messages';
+import * as express from 'express';
+import { ApiResponse, CustomErrorBody } from 'types';
+import { HttpError } from 'routing-controllers';
+import { Config } from '../config';
+import { logger as log } from '../utils/Logger';
+
+export function handleError(error: Error,
+  request: express.Request,
+  response: express.Response,
+  next: express.NextFunction) {
+  const { name, message, stack } = error;
+  const httpCode = error instanceof HttpError ? error.httpCode : 500;
+  const errorBody: CustomErrorBody = {
+    ...error, // in case a library throws its own error (e.g. class-validator)
+    name,
+    message,
+    httpCode,
+  };
+  if (Config.isDevelopment) {
+    errorBody.stack = stack;
+  }
+  const errorResponse: ApiResponse = { error: errorBody };
+  log.warn('%s [request %s]: %s [%d]: %s \n%s\n', new Date(), request.trace, name, httpCode, message, stack);
+  response.status(httpCode).json(errorResponse);
+  next();
+}
