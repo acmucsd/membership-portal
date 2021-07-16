a9834d4d2b19e3889423ffdae9c2f11c23041bf8
Matei-Alexandru Gardus <mateialexandru.gardu@servicenow.com>
2021-07-16 16:56:28 -0700

    Adjust test suite to account for factory bug
    Account creation can occur after event attendance, causing activities
    to be out of order chronologically. Guarantee order in activities by
    introducing retroactive attendance in a future event.
    
    Bug in factory is not a reproducible issue in production, since account
    creation will always occur before event attendance in real time.

--- tests/admin.test.ts
+++ tests/admin.test.ts
@@ -57,7 +57,11 @@ describe('retroactive attendance submission', () => {
     const conn = await DatabaseConnection.get();
     const [user] = UserFactory.create(1);
     const [admin] = UserFactory.with({ accessType: UserAccessType.ADMIN });
-    const [event] = EventFactory.create(1);
+    // Create event in the future to guarantee account creation does not
+    // have a later timestamp than any possible event attendance.
+    //
+    // Not reproducible in production.
+    const event = EventFactory.fakeFutureEvent();
 
     await new PortalState()
       .createUsers([user, admin])
