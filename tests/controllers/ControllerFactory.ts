65cea967e356df5944c36a5418b146c4e2bc654e
Sumeet Bansal <sumeetbansal@gmail.com>
2021-10-18 07:55:50 -0700

    Update ControllerFactory.ts

--- tests/controllers/ControllerFactory.ts
+++ tests/controllers/ControllerFactory.ts
@@ -17,89 +17,49 @@ import EventService from '../../services/EventService';
 import MerchStoreService from '../../services/MerchStoreService';
 
 export class ControllerFactory {
-  private static userController: UserController = null;
-
-  private static feedbackController: FeedbackController = null;
-
-  private static adminController: AdminController = null;
-
-  private static attendanceController: AttendanceController = null;
-
-  private static authController: AuthController = null;
-
-  private static eventController: EventController = null;
-
-  private static leaderboardController: LeaderboardController = null;
-
-  private static merchStoreController: MerchStoreController = null;
-
   public static user(conn: Connection): UserController {
-    if (!ControllerFactory.userController) {
-      const userAccountService = new UserAccountService(conn.manager);
-      const storageService = new StorageService();
-      ControllerFactory.userController = new UserController(userAccountService, storageService);
-    }
-    return ControllerFactory.userController;
+    const userAccountService = new UserAccountService(conn.manager);
+    const storageService = new StorageService();
+    return new UserController(userAccountService, storageService);
   }
 
   public static feedback(conn: Connection): FeedbackController {
-    if (!ControllerFactory.feedbackController) {
-      const feedbackService = new FeedbackService(conn.manager);
-      ControllerFactory.feedbackController = new FeedbackController(feedbackService);
-    }
-    return ControllerFactory.feedbackController;
+    const feedbackService = new FeedbackService(conn.manager);
+    return new FeedbackController(feedbackService);
   }
 
   public static admin(conn: Connection): AdminController {
-    if (!ControllerFactory.adminController) {
-      const userAccountService = new UserAccountService(conn.manager);
-      const storageService = new StorageService();
-      const attendanceService = new AttendanceService(conn.manager);
-      ControllerFactory.adminController = new AdminController(storageService, userAccountService, attendanceService);
-    }
-    return ControllerFactory.adminController;
+    const userAccountService = new UserAccountService(conn.manager);
+    const storageService = new StorageService();
+    const attendanceService = new AttendanceService(conn.manager);
+    return new AdminController(storageService, userAccountService, attendanceService);
   }
 
   public static attendance(conn: Connection): AttendanceController {
-    if (!ControllerFactory.attendanceController) {
-      const attendanceService = new AttendanceService(conn.manager);
-      ControllerFactory.attendanceController = new AttendanceController(attendanceService);
-    }
-    return ControllerFactory.attendanceController;
+    const attendanceService = new AttendanceService(conn.manager);
+    return new AttendanceController(attendanceService);
   }
 
   public static auth(conn: Connection, emailService: EmailService): AuthController {
-    if (!ControllerFactory.authController) {
-      const userAccountService = new UserAccountService(conn.manager);
-      const userAuthService = new UserAuthService(conn.manager);
-      ControllerFactory.authController = new AuthController(userAccountService, userAuthService, emailService);
-    }
-    return ControllerFactory.authController;
+    const userAccountService = new UserAccountService(conn.manager);
+    const userAuthService = new UserAuthService(conn.manager);
+    return new AuthController(userAccountService, userAuthService, emailService);
   }
 
   public static event(conn: Connection): EventController {
-    if (!ControllerFactory.eventController) {
-      const eventService = new EventService(conn.manager);
-      const storageService = new StorageService();
-      const attendanceService = new AttendanceService(conn.manager);
-      ControllerFactory.eventController = new EventController(eventService, storageService, attendanceService);
-    }
-    return ControllerFactory.eventController;
+    const eventService = new EventService(conn.manager);
+    const storageService = new StorageService();
+    const attendanceService = new AttendanceService(conn.manager);
+    return new EventController(eventService, storageService, attendanceService);
   }
 
   public static leaderboard(conn: Connection): LeaderboardController {
-    if (!ControllerFactory.leaderboardController) {
-      const userAccountService = new UserAccountService(conn.manager);
-      ControllerFactory.leaderboardController = new LeaderboardController(userAccountService);
-    }
-    return ControllerFactory.leaderboardController;
+    const userAccountService = new UserAccountService(conn.manager);
+    return new LeaderboardController(userAccountService);
   }
 
   public static merchStore(conn: Connection): MerchStoreController {
-    if (!ControllerFactory.merchStoreController) {
-      const merchStoreService = new MerchStoreService(conn.manager);
-      ControllerFactory.merchStoreController = new MerchStoreController(merchStoreService);
-    }
-    return ControllerFactory.merchStoreController;
+    const merchStoreService = new MerchStoreService(conn.manager);
+    return new MerchStoreController(merchStoreService);
   }
 }
