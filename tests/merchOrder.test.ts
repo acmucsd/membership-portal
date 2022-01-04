593227de9adb2e4192ca3477e2d95c7d21332c49
Michael Shao <56284221+michl1001@users.noreply.github.com>
2022-01-04 00:47:15 -0500

    Adds checks to email address for store access (#243)
    * Adds checks to email address for store access
    
    * Fixes Tests to use ucsd.edu faker emails
    
    * PR fixes
    
    * Lint fixes
    
    * fix package-lock.json
    
    * Added tests for email checking
    
    * Test Fixes and Permission checks
    
    * lint fixes
    
    Co-authored-by: Shravan Hariharan <shravanhariharan2@gmail.com>

--- tests/merchOrder.test.ts
+++ tests/merchOrder.test.ts
@@ -84,6 +84,42 @@ describe('merch orders', () => {
       .called();
   });
 
+  test('members can only access store with a valid acm or ucsd email', async () => {
+    const conn = await DatabaseConnection.get();
+    const UCSDMember = UserFactory.fake({ credits: 10000 });
+    const ACMBoardMember = UserFactory.fake({
+      email: 'random@acmucsd.org',
+      credits: 10000,
+    });
+    const invalidMember = UserFactory.fake({
+      email: 'random@gmail.com',
+      credits: 10000,
+    });
+
+    const affordableOption1 = MerchFactory.fakeOption({
+      quantity: 5,
+      price: 2000,
+      discountPercentage: 0,
+    });
+    const pickupEvent = MerchFactory.fakeFutureOrderPickupEvent();
+
+    await new PortalState()
+      .createUsers(ACMBoardMember, UCSDMember, invalidMember)
+      .createMerchItemOptions(affordableOption1)
+      .createOrderPickupEvents(pickupEvent)
+      .write();
+
+    const merchStoreController = await ControllerFactory.merchStore(conn);
+
+    const ACMBoardMemberResponse = await merchStoreController.getAllMerchCollections(ACMBoardMember);
+    expect(ACMBoardMemberResponse.error).toBe(null);
+
+    const UCSDMemberResponse = await merchStoreController.getAllMerchCollections(UCSDMember);
+    expect(UCSDMemberResponse.error).toBe(null);
+
+    expect(merchStoreController.getAllMerchCollections(invalidMember)).rejects.toThrow(ForbiddenError);
+  });
+
   test('members can cancel orders that they\'ve placed and receive a full refund to their order', async () => {
     const conn = await DatabaseConnection.get();
     const member = UserFactory.fake({ credits: 10000 });
