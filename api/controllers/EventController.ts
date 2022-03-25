3fdf160f13221805f92721226137e8d289c5cb30
shanekim28 <shanekim28@gmail.com>
2022-03-25 12:54:04 -0700

    Update return type for getAllEvents (#295)
    * Update return type for getAllEvents
    
    * Fixed linting issues
    
    Co-authored-by: Shravan Hariharan <33337209+shravanhariharan2@users.noreply.github.com>

--- api/controllers/EventController.ts
+++ api/controllers/EventController.ts
@@ -17,6 +17,7 @@ import {
   GetOneEventResponse,
   UpdateEventCoverResponse,
   GetFutureEventsResponse,
+  GetAllEventsResponse,
   GetPastEventsResponse,
 } from '../../types';
 import { UuidParam } from '../validators/GenericRequests';
@@ -110,7 +111,8 @@ export class EventController {
 
   @UseBefore(OptionalUserAuthentication)
   @Get()
-  async getAllEvents(@QueryParams() options: EventSearchOptions, @AuthenticatedUser() user: UserModel) {
+  async getAllEvents(@QueryParams() options: EventSearchOptions, @AuthenticatedUser() user: UserModel):
+  Promise<GetAllEventsResponse> {
     const canSeeAttendanceCode = !!user && PermissionsService.canEditEvents(user);
     const events = await this.eventService.getAllEvents(canSeeAttendanceCode, options);
     return { error: null, events };
