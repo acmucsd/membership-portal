be5e57538fa24304c941b150e1a3f8716856e4cd
dowhep <caogangshen@gmail.com>
2022-08-03 19:57:19 -0700

    create migration for resume opted in option in table

---
+++ migrations/0035-add-resume-opted-in-table.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+const TABLE_NAME = 'Users';
+
+export class AddResumeOptedInTable1659580706753 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(TABLE_NAME, new TableColumn({
+      name: 'resumeOptedIn',
+      type: 'boolean',
+      isNullable: false,
+      default: false,
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn(TABLE_NAME, 'resumeOptedIn');
+  }
+}
