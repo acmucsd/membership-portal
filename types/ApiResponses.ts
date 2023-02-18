8a2ad3d19674f4dbee3eb52adf9c6400bbdbdf3d
Marcelo Shen <caogangshen@gmail.com>
2023-02-17 16:19:08 -0800

    added resume delete controller route

--- types/ApiResponses.ts
+++ types/ApiResponses.ts
@@ -406,3 +406,5 @@ export interface PublicResume {
 export interface PatchResumeResponse extends ApiResponse {
   resume: PublicResume;
 }
+
+export interface DeleteResumeResponse extends ApiResponse {}
