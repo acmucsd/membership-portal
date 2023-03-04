4d2fd393b623efd5741f25c5ca5abfa2c73e21d2
Caogang (Marcelo) Shen <CaogangShen@gmail.com>
2023-03-03 16:23:35 -0800

    Update services/ResumeService.ts
    LGTM
    
    Co-authored-by: Shravan Hariharan <33337209+shravanhariharan2@users.noreply.github.com>

--- services/ResumeService.ts
+++ services/ResumeService.ts
@@ -66,7 +66,7 @@ export default class ResumeService {
     return this.transactions.readWrite(async (txn) => {
       const resumeRepository = Repositories.resume(txn);
       const resume = await resumeRepository.findByUuid(uuid);
-      if (!resume) throw new NotFoundError('Cannot find a resume of given uuid');
+      if (!resume) throw new NotFoundError('Resume not found');
       if (resume.user.uuid !== user.uuid) throw new ForbiddenError('Cannot delete a resume of another user');
       await resumeRepository.deleteResume(resume);
       return resume;
