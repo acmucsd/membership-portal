47463a789a1848c906a3865c8da24a0fab0f1dbf
Marcelo Shen <caogangshen@gmail.com>
2023-05-06 23:01:13 -0700

    almost completed the query

--- migrations/0036-add-merch-item-image-table.ts
+++ migrations/0036-add-merch-item-image-table.ts
@@ -1,10 +1,11 @@
-import {MigrationInterface, QueryRunner, Table, TableIndex } from "typeorm";
+import {MigrationInterface, QueryRunner, Table, TableColumn, TableIndex } from "typeorm";
 
-const TABLE_NAME = 'MerchItemImages';
+const TABLE_NAME = 'MerchandiseItemPhotos';
+const MERCH_TABLE_NAME = 'Merchandise';
 
 export class addMerchItemImageTable1681777109787 implements MigrationInterface {
-
   public async up(queryRunner: QueryRunner): Promise<void> {
+    // instantiates table with columns: uuid, merchItem, picture
     await queryRunner.createTable(new Table({
       name: TABLE_NAME,
       columns: [
@@ -26,17 +27,37 @@ export class addMerchItemImageTable1681777109787 implements MigrationInterface {
       ],
     }));
 
+    // optimize searching
     await queryRunner.createIndices(TABLE_NAME, [
       new TableIndex({
         name: 'images_by_item_index',
         columnNames: ['merchItem'],
       }),
     ]);
+
+    // add images from each item of the merchandise table to the photo table
+    await queryRunner.query(
+      `INSERT INTO "${TABLE_NAME}" (merchItem, picture) ` +
+      `SELECT uuid, picture FROM "${MERCH_TABLE_NAME}"`
+    );
+
+    // remove the column from the old table
+    await queryRunner.dropColumn(`"${MERCH_TABLE_NAME}"`, 'picture');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // create old column
-    // fill old column with the first image from each table
+    // create old column (copied from migration #7)
+    await queryRunner.addColumn(`"${MERCH_TABLE_NAME}"`, new TableColumn({
+      name: 'picture',
+      type: 'varchar(255)',
+      isNullable: true,
+    }));
+
+    // fill old column with the first image from the photo table
+    await queryRunner.query(
+      `ALTER TABLE "${MERCH_TABLE_NAME}" ALTER COLUMN picture ` +
+      `SELECT picture FROM "${TABLE_NAME}" GROUP BY merchItem`
+    );
   }
 
 }
