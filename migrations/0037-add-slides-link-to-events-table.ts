1e95462b509674b5544c7812353794faf7a4de81
Nikhil Dange <nsdange@ucsd.edu>
2023-10-06 12:11:08 -0700

    linting :(

--- migrations/0037-add-slides-link-to-events-table.ts
+++ migrations/0037-add-slides-link-to-events-table.ts
@@ -1,19 +1,17 @@
-import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-const TABLE_NAME = "Events";
+const TABLE_NAME = 'Events';
 
-export class addSlidesLinkToEventsTable1696465672671 implements MigrationInterface {
+export class AddSlidesLinkToEventsTable1696465672671 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(TABLE_NAME, new TableColumn({
+      name: 'slidesLink',
+      type: 'text',
+      isNullable: true,
+    }));
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.addColumn(TABLE_NAME, new TableColumn({
-          name: 'slidesLink',
-          type: 'text',
-          isNullable: true,
-        }));
-      }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropColumn(TABLE_NAME, 'slides_link');
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn(TABLE_NAME, 'slides_link');
+  }
 }
