e86e9429ca67d93d92781a229fd5887c5f116314
iAmWeirdTheFifth <maxwn04@gmail.com>
2023-10-10 19:24:20 -0700

    migration name fix

--- migrations/0037-add-collection-image-table.ts
+++ migrations/0037-add-collection-image-table.ts
@@ -1,50 +1,50 @@
-import { MigrationInterface, QueryRunner, Table, TableColumn, TableIndex } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
 
 const TABLE_NAME = 'MerchCollectionPhotos';
 
-export class addCollectionImageTable implements MigrationInterface {
+export class addCollectionImageTable1696990832868 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.createTable(new Table({
-        name: TABLE_NAME,
-        columns: [
-          {
-            name: 'uuid',
-            type: 'uuid',
-            isGenerated: true,
-            isPrimary: true,
-            generationStrategy: 'uuid',
-          },
-          {
-            name: 'merchCollection',
-            type: 'uuid',
-          },
-          {
-            name: 'uploadedPhoto',
-            type: 'varchar(255)',
-          },
-          {
-            name: 'uploadedAt',
-            type: 'timestamptz',
-            default: 'CURRENT_TIMESTAMP(6)',
-          },
-          {
-            name: 'position',
-            type: 'integer',
-          },
-        ],
-      }));
+        await queryRunner.createTable(new Table({
+          name: TABLE_NAME,
+          columns: [
+            {
+              name: 'uuid',
+              type: 'uuid',
+              isGenerated: true,
+              isPrimary: true,
+              generationStrategy: 'uuid',
+            },
+            {
+              name: 'merchCollection',
+              type: 'uuid',
+            },
+            {
+              name: 'uploadedPhoto',
+              type: 'varchar(255)',
+            },
+            {
+              name: 'uploadedAt',
+              type: 'timestamptz',
+              default: 'CURRENT_TIMESTAMP(6)',
+            },
+            {
+              name: 'position',
+              type: 'integer',
+            },
+          ],
+        }));
 
-      await queryRunner.createIndices(TABLE_NAME, [
-        new TableIndex({
-          name: 'images_by_collection_index',
-          columnNames: ['merchCollection'],
-        }),
-      ]);
-    }
+        await queryRunner.createIndices(TABLE_NAME, [
+          new TableIndex({
+            name: 'images_by_collection_index',
+            columnNames: ['merchCollection'],
+          }),
+        ]);
+      }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropTable(TABLE_NAME);
-    }
+      public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable(TABLE_NAME);
+      }
 
 }
