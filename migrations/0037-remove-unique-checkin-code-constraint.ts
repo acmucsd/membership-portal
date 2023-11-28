1a53559d9ec263c891dc124fa4d6d39785217a47
Nikhil Dange <nsdange@ucsd.edu>
2023-11-27 23:38:32 -0800

    added sample db migration for testing

---
+++ migrations/0037-remove-unique-checkin-code-constraint.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+const TABLE_NAME = 'Events';
+const CONSTRAINT_NAME = 'Events_attendanceCode_key';
+
+export class removeUniqueCheckinCodeConstraint1701156281658 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "${TABLE_NAME}" DROP CONSTRAINT "${CONSTRAINT_NAME}"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "${TABLE_NAME}" ADD UNIQUE ("attendanceCode")`);
+    }
+}
