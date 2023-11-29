048e9805f0819ae569190a451bf3f90152b3b55b
Nikhil Dange <nikhil@acmucsd.org>
2023-11-28 18:34:46 -0800

    User Social Media Link Seed Data (#365)
    * social media seed data
    
    * uh
    
    * done
    
    * cleanup
    
    * linting is the death of me

--- tests/data/index.ts
+++ tests/data/index.ts
@@ -3,6 +3,7 @@ export * from './DatabaseConnection';
 export * from './UserFactory';
 export * from './EventFactory';
 export * from './MerchFactory';
+export * from './UserSocialMediaFactory';
 export * from './ResumeFactory';
 
 export * from './PortalState';
