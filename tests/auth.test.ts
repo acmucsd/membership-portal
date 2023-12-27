2cf55f48fd0f3f65366be3af117ea82775e83204
Max Weng <maxwn04@gmail.com>
2023-12-26 17:15:40 -0800

    tests

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -59,6 +59,7 @@ describe('account registration', () => {
       uuid: registerResponse.user.uuid,
       profilePicture: null,
       userSocialMedia: [],
+      isAttendancePublic: true
     });
 
     // check that email verification is sent
