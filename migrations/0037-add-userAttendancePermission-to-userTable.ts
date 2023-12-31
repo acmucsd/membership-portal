03a4c3b1fec6125ea188c43da227d31ed5ff9895
Max Weng <73797155+maxwn04@users.noreply.github.com>
2023-12-31 10:29:04 -0800

    Get another user's event attendance (#358)
    * attendences from user uuid
    
    * lint and bugfix
    
    * check same user
    
    * controller factory changes
    
    * lint fixes
    
    * unit test for get attendance by uuid
    
    * lint
    
    * add permision
    
    * add types
    
    * add everything else
    
    * rename migrtion
    
    * test when permision is off
    
    * lint
    
    * forgor to add
    
    * change permission name and fix logic a bit
    
    * rename permission, change patch user
    
    * lint fix
    
    * lint fix
    
    * oops
    
    * check user exists
    
    * lint
    
    * rename tests
    
    * public profile change
    
    * change user model
    
    * lint
    
    * tests
    
    * lint
    
    * updated api version
    
    ---------
    
    Co-authored-by: Nikhil Dange <nsdange@ucsd.edu>

---
+++ migrations/0037-add-userAttendancePermission-to-userTable.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+const TABLE_NAME = 'Users';
+
+export class AddUserAttendancePermissionToUserTable1691286073346 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(TABLE_NAME, new TableColumn({
+      name: 'isAttendancePublic',
+      type: 'boolean',
+      default: true,
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn(TABLE_NAME, 'isAttendancePublic');
+  }
+}
