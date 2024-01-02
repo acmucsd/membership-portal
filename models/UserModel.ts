851f4349a6455576d7e0e57f45a02f75e626c48e
Faris Ashai <farisashai@gmail.com>
2024-01-01 23:38:28 -0800

    lint

--- models/UserModel.ts
+++ models/UserModel.ts
@@ -153,7 +153,7 @@ export class UserModel extends BaseEntity {
       points: this.points,
       credits: this.credits,
       isAttendancePublic: this.isAttendancePublic,
-      userSocialMedia: []
+      userSocialMedia: [],
     };
     if (this.userSocialMedia) {
       fullUserProfile.userSocialMedia = this.userSocialMedia.map((sm) => sm.getPublicSocialMedia());
