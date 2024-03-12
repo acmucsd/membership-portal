f2200ec9c5250530383c24c63874b167e56ec45e
Nikhil Dange <nikhil@acmucsd.org>
2024-03-11 22:34:41 -0700

    Remove lastLogin column–staging (#416)
    * Remove lastLogin column from Users table
    
    * recreated migration file
    
    ---------
    
    Co-authored-by: Shravan Hariharan <shravanhariharan2@gmail.com>
    Co-authored-by: Shravan Hariharan <33337209+shravanhariharan2@users.noreply.github.com>

---
+++ migrations/0042-remove-users-lastLogin-column.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+const TABLE_NAME = 'Users';
+
+export class RemoveUsersLastLoginColumn1710221027614 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn(TABLE_NAME, 'lastLogin');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(TABLE_NAME, new TableColumn({
+      name: 'lastLogin',
+      type: 'timestamptz',
+      default: 'CURRENT_TIMESTAMP(6)',
+    }));
+  }
+}
