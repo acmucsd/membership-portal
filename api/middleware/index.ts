8d7425db56a0b405e3bf83895a1c31a137bafbd4
Nikhil Dange <nsdange@ucsd.edu>
2024-03-12 18:19:44 -0700

    removed rate limiter stuff

--- api/middleware/index.ts
+++ api/middleware/index.ts
@@ -2,12 +2,10 @@ import { RequestLogger } from './RequestLogger';
 import { ErrorHandler } from './ErrorHandler';
 import { NotFoundHandler } from './NotFoundHandler';
 import { MetricsRecorder } from './MetricsRecorder';
-import { RateLimiter } from './RateLimiter';
 
 export const middlewares = [
   ErrorHandler,
   NotFoundHandler,
   RequestLogger,
   MetricsRecorder,
-  RateLimiter,
 ];
