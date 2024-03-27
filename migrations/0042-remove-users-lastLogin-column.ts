abe5563219bed1cedfb60d4441b3c325b58130fe
Nikhil Dange <nsdange@ucsd.edu>
2024-03-26 23:09:27 -0700

    migration and edited model

---
+++ migrations/0042-remove-users-lastLogin-column.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+const TABLE_NAME = 'Users';
+
+export class RemoveUsersLastLoginColumn1711518997063 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn(TABLE_NAME, 'lastLogin');
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(TABLE_NAME, new TableColumn({
+      name: 'lastLogin',
+      type: 'timestampz',
+      default: 'CURRENT_TIMESTAMP(6)',
+    }));
+  }
+}
