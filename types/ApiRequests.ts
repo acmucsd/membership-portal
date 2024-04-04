a7a674b88321d49787dc982d21f78a1948ec82fa
Eric Chang <55172722+echang594@users.noreply.github.com>
2024-04-03 17:41:36 -0700

    Add Discord and Google Calendar Event ID columns (#415)
    * Added discord and google calendar event columns
    
    * Fix event model
    
    * Made names less verbose
    
    * Recreate migration
    
    * Changed name from field to column
    
    * Renumber migration

--- types/ApiRequests.ts
+++ types/ApiRequests.ts
@@ -155,6 +155,8 @@ export interface OptionalEventProperties {
   eventLink?: string;
   requiresStaff?: boolean;
   staffPointBonus?: number;
+  discordEvent?: Uuid;
+  googleCalendarEvent?: Uuid;
 }
 
 export interface Event extends OptionalEventProperties {
