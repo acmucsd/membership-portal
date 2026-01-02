791ed251e1cda72f99ee399f3b286b7c2e2dc8a0
Nandita <95146588+NanditaRK@users.noreply.github.com>
2026-01-02 15:12:30 -0800

    moving all the public types in the services layer to the controller layer (#473)

--- services/MerchStoreService.ts
+++ services/MerchStoreService.ts
@@ -11,12 +11,9 @@ import {
   MerchItem,
   MerchItemOption,
   MerchItemEdit,
-  PublicMerchItemOption,
   OrderStatus,
   PublicMerchItemWithPurchaseLimits,
-  PublicMerchItemPhoto,
   MerchItemPhoto,
-  PublicMerchCollectionPhoto,
   MerchCollectionPhoto,
 } from '../types';
 import { MerchandiseItemModel } from '../models/MerchandiseItemModel';
@@ -68,25 +65,26 @@ export default class MerchStoreService {
     });
   }
 
-  public async findCollectionByUuid(uuid: Uuid, canSeeInactiveCollections = false): Promise<PublicMerchCollection> {
+  public async findCollectionByUuid(uuid: Uuid,
+     canSeeInactiveCollections = false): Promise<MerchandiseCollectionModel> {
     const collection = await this.transactions.readOnly(async (txn) => Repositories
       .merchStoreCollection(txn)
       .findByUuid(uuid));
     if (!collection) throw new NotFoundError('Merch collection not found');
     if (collection.archived && !canSeeInactiveCollections) throw new ForbiddenError();
     collection.collectionPhotos = collection.collectionPhotos.sort((a, b) => a.position - b.position);
-    return canSeeInactiveCollections ? collection : collection.getPublicMerchCollection();
+    return collection;
   }
 
   public async getAllCollections(canSeeInactiveCollections = false, canSeeHiddenItems = canSeeInactiveCollections):
-  Promise<PublicMerchCollection[]> {
+  Promise<MerchandiseCollectionModel[]> {
     return this.transactions.readOnly(async (txn) => {
       const merchCollectionRepository = Repositories.merchStoreCollection(txn);
       if (canSeeInactiveCollections) {
         return merchCollectionRepository.getAllCollections();
       }
       const collections = await merchCollectionRepository.getAllActiveCollections();
-      return collections.map((c) => c.getPublicMerchCollection(canSeeHiddenItems));
+      return collections;
     });
   }
 
@@ -178,7 +176,7 @@ export default class MerchStoreService {
    * @returns created collection photo
   */
   public async createCollectionPhoto(collection: Uuid, properties: MerchCollectionPhoto):
-  Promise<PublicMerchCollectionPhoto> {
+  Promise<MerchCollectionPhotoModel> {
     return this.transactions.readWrite(async (txn) => {
       const merchCollectionRepository = Repositories.merchStoreCollection(txn);
       const merchCollection = await merchCollectionRepository.findByUuid(collection);
@@ -192,7 +190,7 @@ export default class MerchStoreService {
       MerchStoreService.verifyCollectionHasValidPhotos(merchCollection);
 
       const upsertedPhoto = await merchStoreCollectionPhotoRepository.upsertCollectionPhoto(createdPhoto);
-      return upsertedPhoto.getPublicMerchCollectionPhoto();
+      return upsertedPhoto;
     });
   }
 
@@ -359,7 +357,7 @@ export default class MerchStoreService {
    * @param option merch item option
    * @returns created item option
    */
-  public async createItemOption(item: Uuid, option: MerchItemOption): Promise<PublicMerchItemOption> {
+  public async createItemOption(item: Uuid, option: MerchItemOption): Promise<MerchandiseItemOptionModel> {
     return this.transactions.readWrite(async (txn) => {
       const merchItem = await Repositories.merchStoreItem(txn).findByUuid(item);
       if (!merchItem) throw new NotFoundError('Merch item not found');
@@ -370,7 +368,7 @@ export default class MerchStoreService {
       MerchStoreService.verifyItemHasValidOptions(merchItem);
 
       const upsertedOption = await merchItemOptionRepository.upsertMerchItemOption(createdOption);
-      return upsertedOption.getPublicMerchItemOption();
+      return upsertedOption;
     });
   }
 
@@ -415,7 +413,7 @@ export default class MerchStoreService {
    * @param properties merch item photo picture url and position
    * @returns created item photo
    */
-  public async createItemPhoto(item: Uuid, properties: MerchItemPhoto): Promise<PublicMerchItemPhoto> {
+  public async createItemPhoto(item: Uuid, properties: MerchItemPhoto): Promise<MerchandiseItemPhotoModel> {
     return this.transactions.readWrite(async (txn) => {
       const merchItem = await Repositories.merchStoreItem(txn).findByUuid(item);
       if (!merchItem) throw new NotFoundError('Merch item not found');
@@ -428,7 +426,7 @@ export default class MerchStoreService {
       MerchStoreService.verifyItemHasValidPhotos(merchItem);
 
       const upsertedPhoto = await merchStoreItemPhotoRepository.upsertMerchItemPhoto(createdPhoto);
-      return upsertedPhoto.getPublicMerchItemPhoto();
+      return upsertedPhoto;
     });
   }
 
